bb93ad24dd3289eec1a2df65cce5a070271cdc2f
algorithm0x02 <algorithm0x02@gmail.com>
2020-11-18 23:29:00 +0900

    main pages outline

---
+++ lib/detail_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+class DetailPage extends StatefulWidget {
+  @override
+  _DetailPageState createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'this is a detail page',
+          style: TextStyle(color:  Colors.blue, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: _buildBody(),
+    );
+  }
+}
+
+  Widget _buildBody() {
+    return Padding(padding: EdgeInsets.all(8.0),
+    );
+  }
+}
+
